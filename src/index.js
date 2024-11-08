f38ed06154572a895ac719732a4f33ac14b1c416
deucenn <panix.black@web.de>
2024-11-08 12:25:31 +0100

    Added a forecast

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./styles.css"
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import callWeatherApi from "./modules/API";
 import Weather from "./modules/Weather";
 
 Weather();
--- src/modules/Weather.js
+++ src/modules/Weather.js
@@ -1,5 +1,6 @@
 import callWeatherApi from "./API";
 import { FahrenheitToCelsius, MphToKmh } from "./Conversion";
+import { getWeatherIcon, getCardinalDirection } from "./HelperFunctions";
 
 const locationInput = document.querySelector(".search-input");
 const locationInputBtn = document.querySelector(".search-submit");
@@ -16,71 +17,94 @@ const uvIndex = document.querySelector(".data-info-uv-index");
 const pressure = document.querySelector(".data-info-pressure");
 const windDirection = document.querySelector(".data-info-wind-direction");
 const windSpeed = document.querySelector(".data-info-wind-speed");
+const tomorrowMaxTemp = document.querySelector(".data-tomorrow-max-temp");
+const tomorrowMinTemp = document.querySelector(".data-tomorrow-min-temp");
+const overtomorrowMaxTemp = document.querySelector(
+  ".data-overtomorrow-max-temp"
+);
+const overtomorrowMinTemp = document.querySelector(
+  ".data-overtomorrow-min-temp"
+);
 
-function getCardinalDirection(degrees) {
-    if (degrees >= 0 && degrees < 90) return `<i class="fas fa-arrow-up"></i>`; // North
-    else if (degrees >= 90 && degrees < 180) return `<i class="fas fa-arrow-right"></i>`; // East
-    else if (degrees >= 180 && degrees < 270) return `<i class="fas fa-arrow-down"></i>`; // South
-    else if (degrees >= 270 && degrees < 360) return `<i class="fas fa-arrow-left"></i>`; // West
-    return `<i class="fas fa-question-circle"></i>`; // Unknown direction
-  }
+function updateWeatherUI(data) {
+  const [hourSunrise, minuteSunrise] = data.sunrise.split(":");
+  const [hourSunset, minuteSunset] = data.sunset.split(":");
+
+  // Update the UI with the fetched data
+  city.innerHTML = `<h1>${data.city}</h1>`;
+  cityDetails.innerHTML = `N${data.latitude}° E${data.longitude}°`;
+  temp.innerHTML = `<h1>${getWeatherIcon(data.icon)} ${FahrenheitToCelsius(
+    data.temp
+  )}°C</h1>`;
+  tempDesc.innerHTML = `
+    <p>Feels like ${FahrenheitToCelsius(data.feelsLike)}°C</p>
+    <p>${data.conditions}</p>`;
+
+  minTemp.innerHTML = `
+      <h3>Min. Temperature</h3>
+      <h4>${FahrenheitToCelsius(data.tempMin)}°C</h4>`;
+
+  maxTemp.innerHTML = `
+      <h3>Max. Temperature</h3>
+      <h4>${FahrenheitToCelsius(data.tempMax)}°C</h4>`;
+
+  humidity.innerHTML = `
+      <h3>Humidity</h3>
+      <h4>${data.humidity}%</h4>`;
+
+  sunrise.innerHTML = `
+      <h3>Sunrise</h3>
+      <h4>${hourSunrise}:${minuteSunrise}</h4>`;
+
+  sunset.innerHTML = `
+      <h3>Sunset</h3>
+      <h4>${hourSunset}:${minuteSunset}</h4>`;
+
+  uvIndex.innerHTML = `
+      <h3>UV Index</h3>
+      <h4>${data.uvIndex}</h4>`;
+
+  pressure.innerHTML = `
+      <h3>Pressure</h3>
+      <h4>${data.pressure} hPa</h4>`;
+
+  windDirection.innerHTML = `
+      <h3>Wind Direction</h3>
+      <h4>${getCardinalDirection(data.windDirection)}</h4>`;
+
+  windSpeed.innerHTML = `
+      <h3>Wind Speed</h3>
+      <h4>${MphToKmh(data.windSpeed)} km/h</h4>`;
+
+  tomorrowMaxTemp.innerHTML = `
+        ${getWeatherIcon(data.tomorrowIcon)} ${FahrenheitToCelsius(
+    data.tomorrowMaxTemp
+  )}°C`;
+
+  tomorrowMinTemp.innerHTML = `${FahrenheitToCelsius(data.tomorrowMinTemp)}°C`;
+
+  overtomorrowMaxTemp.innerHTML = `${getWeatherIcon(
+    data.overtomorrowIcon
+  )} ${FahrenheitToCelsius(data.overtomorrowMaxTemp)}°C`;
+
+  overtomorrowMinTemp.innerHTML = `${FahrenheitToCelsius(
+    data.overtomorrowMinTemp
+  )}°C`;
+}
 
 export default function Weather() {
+  const defaultLocation = "Solingen";
+  callWeatherApi(defaultLocation)()
+    .then(updateWeatherUI)
+    .catch((error) => {
+      console.error("Error fetching weather data for default location:", error);
+    });
   locationInputBtn.addEventListener("click", () => {
     const location = locationInput.value;
-    const getWeatherData = callWeatherApi(location);
-    getWeatherData()
-      .then((data) => {
-        console.log(data);
-
-        const [hourSunrise, minuteSunrise] = data.sunrise.split(":");
-        const [hourSunset, minuteSunset] = data.sunset.split(":");
-
-        // Update the UI with the fetched data
-        city.innerHTML = `<h1>${data.city}</h1>`;
-        cityDetails.innerHTML = `N${data.latitude}° E${data.longitude}°`;
-        temp.innerHTML = `<h1>${FahrenheitToCelsius(data.temp)}°C</h1>`;
-        tempDesc.innerHTML = `<p>${data.conditions}</p>`;
-
-        minTemp.innerHTML = `
-          <h3>Min. Temperature</h3>
-          <h4>${FahrenheitToCelsius(data.tempMin)}°C</h4>`;
-
-        maxTemp.innerHTML = `
-          <h3>Max. Temperature</h3>
-          <h4>${FahrenheitToCelsius(data.tempMax)}°C</h4>`;
-
-        humidity.innerHTML = `
-          <h3>Humidity</h3>
-          <h4>${data.humidity}%</h4>`;
-
-        sunrise.innerHTML = `
-          <h3>Sunrise</h3>
-          <h4>${hourSunrise}:${minuteSunrise}</h4>`;
-
-        sunset.innerHTML = `
-          <h3>Sunset</h3>
-          <h4>${hourSunset}:${minuteSunset}</h4>`;
-
-        uvIndex.innerHTML = `
-          <h3>UV Index</h3>
-          <h4>${data.uvIndex}</h4>`;
-
-        pressure.innerHTML = `
-          <h3>Pressure</h3>
-          <h4>${data.pressure} hPa</h4>`;
-
-        windDirection.innerHTML = `
-          <h3>Wind Direction</h3>
-          <h4>${getCardinalDirection(data.windDirection)}</h4>`;
-
-        windSpeed.innerHTML = `
-          <h3>Wind Speed</h3>
-          <h4>${MphToKmh(data.windSpeed)} km/h</h4>`;
-      })
+    callWeatherApi(location)()
+      .then(updateWeatherUI)
       .catch((error) => {
         console.error("Error fetching weather data:", error);
       });
   });
 }
-
